cce3ef68ca5478218c54815ab82f19d5af686176
Michael E. Ershov <em.ershov@yandex.ru>
2023-07-14 15:03:51 +0800

    Added .env

--- src/top-page/top-page.controller.ts
+++ src/top-page/top-page.controller.ts
@@ -10,14 +10,19 @@ import {
 } from '@nestjs/common';
 import { TopPageModel } from './top-page.model';
 import { FindTopPageDto } from './dto/find-top-page.dto';
+import { ConfigService } from '@nestjs/config';
 
 @Controller('top-page')
 export class TopPageController {
+  constructor(private readonly configService: ConfigService) {}
+
   @Post('create')
   async create(@Body() dto: Omit<TopPageModel, '_id'>) {}
 
   @Get(':id')
-  async get(@Param('id') id: string) {}
+  async get(@Param('id') id: string) {
+    this.configService.get('TEST');
+  }
 
   @Delete(':id')
   async delete(@Param('id') id: string) {}
