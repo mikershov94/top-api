70be719e0b44ca5f8b6d8a79f2e66566f9e89ac0
Michael E. Ershov <em@newton.finance>
2023-07-24 09:29:07 +0800

    Fixed mongo-config

--- src/config/mongo.config.ts
+++ src/config/mongo.config.ts
@@ -12,15 +12,15 @@ export const getMongoConfig = async (
 
 const getMongoString = (configService: ConfigService): string =>
   'mongodb://' +
-  configService.get('MONGO_LOGIN') +
-  ':' +
-  configService.get('MONGO_PASSWORD') +
-  '@' +
+  // configService.get('MONGO_LOGIN') +
+  // ':' +
+  // configService.get('MONGO_PASSWORD') +
+  // '@' +
   configService.get('MONGO_HOST') +
   ':' +
-  configService.get('MONGO_PORT') +
-  '/' +
-  configService.get('MONGO_AUTHDATABASE');
+  configService.get('MONGO_PORT');
+// '/' +
+// configService.get('MONGO_AUTHDATABASE');
 
 // const getMongoOptions = () => ({
 //   useNewURLParser: true,
