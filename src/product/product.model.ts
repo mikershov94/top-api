dc74da09aac1ebf694ee152a7b5e3605d43af3da
Michael E. Ershov <em@newton.finance>
2023-07-28 13:46:10 +0800

    Added Product model and module

--- src/product/product.model.ts
+++ src/product/product.model.ts
@@ -1,17 +1,53 @@
-export class ProductModel {
-  _id: string;
-  image: string;
-  title: string;
-  price: number;
-  oldPrice: number;
-  credit: number;
-  calculatedRating: number;
-  description: string;
-  advantages: string;
-  disAdvantages: string;
-  categories: string[];
-  tags: string;
-  characteristics: {
-    [key: string]: string;
-  };
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type ProductDocument = HydratedDocument<ProductModel>;
+
+export class ProductCharacteristic {
+  name: string;
+  value: string;
 }
+
+@Schema()
+export class ProductModel {
+  @Prop()
+  _id: string;
+
+  @Prop()
+  image: string;
+
+  @Prop()
+  title: string;
+
+  @Prop()
+  price: number;
+
+  @Prop()
+  oldPrice: number;
+
+  @Prop()
+  credit: number;
+
+  @Prop()
+  calculatedRating: number;
+
+  @Prop()
+  description: string;
+
+  @Prop()
+  advantages: string;
+
+  @Prop()
+  disAdvantages: string;
+
+  @Prop([String])
+  categories: string[];
+
+  @Prop([String])
+  tags: string[];
+
+  @Prop([ProductCharacteristic])
+  characteristics: ProductCharacteristic[];
+}
+
+export const ProductSchema = SchemaFactory.createForClass(ProductModel);
