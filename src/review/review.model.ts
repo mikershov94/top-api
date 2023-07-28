64007436872e39eeb2773644416766912296379f
Michael E. Ershov <em@newton.finance>
2023-07-28 14:08:09 +0800

    Added review model and module

--- src/review/review.model.ts
+++ src/review/review.model.ts
@@ -1,8 +1,26 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type ReviewDocument = HydratedDocument<ReviewModel>;
+@Schema()
 export class ReviewModel {
+  @Prop({ unique: true })
   _id: string;
+
+  @Prop()
   name: string;
+
+  @Prop()
   title: string;
+
+  @Prop()
   description: string;
+
+  @Prop()
   rating: number;
+
+  @Prop()
   createdAt: Date;
 }
+
+export const ReviewSchema = SchemaFactory.createForClass(ReviewModel);
