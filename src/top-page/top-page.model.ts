023a1e90619030aca65aaf00ce5e0ff041027684
Michael E. Ershov <em@newton.finance>
2023-07-28 14:20:05 +0800

    Added top-page module and model

--- src/top-page/top-page.model.ts
+++ src/top-page/top-page.model.ts
@@ -1,3 +1,6 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
 export enum TopLevelCategory {
   Courses,
   Services,
@@ -5,23 +8,51 @@ export enum TopLevelCategory {
   Products,
 }
 
-export class TopPageModel {
-  _id: string;
-  firstCategory: TopLevelCategory;
-  secondCategory: string;
+export class HhData {
+  count: number;
+  juniorSalary: number;
+  middleSalary: number;
+  seniorSalary: number;
+}
+
+export class TopPageAdvantage {
   title: string;
+  description: string;
+}
+
+export type TopPageDocument = HydratedDocument<TopPageModel>;
+
+@Schema()
+export class TopPageModel {
+  @Prop({ enum: TopLevelCategory })
+  firstCategory: TopLevelCategory;
+
+  @Prop()
+  secondCategory: string;
+
+  @Prop({ unique: true })
+  alias: string;
+
+  @Prop()
+  title: string;
+
+  @Prop()
   category: string;
-  hh?: {
-    count: number;
-    juniorSalary: number;
-    middleSalary: number;
-    seniorSalary: number;
-  };
-  advantages: {
-    title: string;
-    description: string;
-  }[];
+
+  @Prop()
+  hh?: HhData;
+
+  @Prop([TopPageAdvantage])
+  advantages: TopPageAdvantage[];
+
+  @Prop()
   seoText: string;
+
+  @Prop()
   tagTitle: string;
+
+  @Prop([String])
   tags: string[];
 }
+
+export const TopPageSchema = SchemaFactory.createForClass(TopPageModel);
--- src/top-page/top-page.module.ts
+++ src/top-page/top-page.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
-import { TopPageController } from './top-page.controller';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { TopPageController } from './top-page.controller';
+import { TopPageModel, TopPageSchema } from './top-page.model';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forFeature([
+      {
+        name: TopPageModel.name,
+        schema: TopPageSchema,
+      },
+    ]),
+  ],
   controllers: [TopPageController],
 })
 export class TopPageModule {}
